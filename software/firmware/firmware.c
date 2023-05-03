61ff2d976dbeb64734294f485d63c9353d9ee9ff
Edw590 <40151365+Edw590@users.noreply.github.com>
2023-05-03 17:11:52 +0100

    replace vga

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -4,6 +4,15 @@
 #include "iob-im.h"
 #include "printf.h"
 
+#define BALL_X_LEN 7
+#define BALL_Y_LEN BALL_X_LEN
+#define BAR_X_LEN 3
+#define BAR_Y_LEN 20
+
+#define BALL_INFO 0
+#define BARL_INFO 1
+#define BARR_INFO 2
+
 char *send_string = "Sending this string as a file to console.\n"
                     "The file is then requested back from console.\n"
                     "The sent file is compared to the received file to confirm " 
@@ -40,20 +49,53 @@ int compare_str(char *str1, char *str2, int str_size) {
     return 0;
 }
 
-struct RGB {
-    int r:4;
-    int g:4;
-    int b:4;
+
+struct ObjInfo {
+    unsigned int x:10;
+    unsigned int y:10;
+    unsigned int unused:12;
 } __attribute__((packed));
 
-void setCoords(int object, int x, int y) {
-    if (0 == object) {
-        im_set_ball_loc(x | (y << 10));
-    } else if (1 == object) {
-        im_set_barl_loc(x | (y << 10));
-    } else if (2 == object) {
-        im_set_barr_loc(x | (y << 10));
+// Ball, BarL, BarR (indexes in macros)
+struct ObjInfo objs[3] = {0};
+
+void setCoords(int obj, int x, int y) {
+    int obj_x_len = 0;
+    int obj_y_len = 0;
+    
+    if (BALL_INFO == obj) {
+        obj_x_len = BALL_X_LEN;
+        obj_y_len = BALL_Y_LEN;
+    } else {
+        obj_x_len = BAR_X_LEN;
+        obj_y_len = BAR_Y_LEN;
     }
+    
+    int new_x = 0;
+    int new_y = 0;
+
+    if (x - obj_x_len < 0 || x + obj_x_len > 639) {
+        new_x = objs[obj].x; // Keep old x
+    } else {
+        new_x = x;
+    }
+    if (y - obj_y_len < 0 || y + obj_y_len > 479) {
+        new_y = objs[obj].y; // Keep old y
+    } else {
+        new_y = y;
+    }
+
+    int new_info = new_x | (new_y << 10);
+
+    if (BALL_INFO == obj) {
+        im_set_ball_loc(new_info);
+    } else if (BARL_INFO == obj) {
+        im_set_barl_loc(new_info);
+    } else if (BARR_INFO == obj) {
+        im_set_barr_loc(new_info);
+    }
+    objs[obj].x = new_x;
+    objs[obj].y = new_y;
 }
 
 int main() {
@@ -62,31 +104,26 @@ int main() {
 
     im_init(IM_BASE);
 
-    int prev_sw_state = im_get_sw_input();
 
-    int ball_x = 320;
-    int ball_y = 239;
-    setCoords(0, ball_x, ball_y);
-    int barl_x = 20;
-    int barl_y = 239;
-    setCoords(1, barl_x, barl_y);
-    int barr_x = 620;
-    int barr_y = 239;
-    setCoords(2, barr_x, barr_y);
+    setCoords(BALL_INFO, 320, 239);    
+    setCoords(BARL_INFO, 20, 239);
+    setCoords(BARR_INFO, 620, 239);
     
+    int prev_sw_state = im_get_sw_input();
+  
     while (true) {
         if (prev_sw_state != im_get_sw_input()) {
             prev_sw_state = im_get_sw_input();
 
             ball_x++;
             ball_y++;
-            setCoords(0, ball_x, ball_y);
+            setCoords(BALL_INFO, ball_x, ball_y);
             barl_x++;
             barl_y++;
-            setCoords(1, barl_x, barl_y);
+            setCoords(BARL_INFO, barl_x, barl_y);
             barr_x++;
             barr_y++;
-            setCoords(2, barr_x, barr_y);
+            setCoords(BARR_INFO, barr_x, barr_y);
         }
     }
     
